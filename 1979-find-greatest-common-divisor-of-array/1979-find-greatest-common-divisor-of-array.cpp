5b739fc087c816a9ebf49ef682088cdb8214954d
rrohitmeenaa <rohitmeena1789@gmail.com>
2026-07-18 08:10:06 +0530

    Time: 1 ms (8.88%), Space: 16.2 MB (67.89%) - LeetHub

--- 1979-find-greatest-common-divisor-of-array/1979-find-greatest-common-divisor-of-array.cpp
+++ 1979-find-greatest-common-divisor-of-array/1979-find-greatest-common-divisor-of-array.cpp
@@ -6,7 +6,7 @@ public:
         int a = arr[0];
         int b = arr[n-1];
         int gcd = 1;
-        for(int i = 2;i<=b;i++){
+        for(int i = 2;i<=a;i++){
             if(a%i==0 && b%i==0){
                 gcd = i;
             }
