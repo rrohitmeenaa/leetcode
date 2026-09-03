aaf9055e231ea3c8fdaca7d36a7325677205a077
rrohitmeenaa <rohitmeena1789@gmail.com>
2026-09-03 08:57:27 +0530

    Time: 0 ms (100%), Space: 8.4 MB (54.4%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int climb(int n,vector<int>& dp){
+        if(n<=2){
+            return n;
+        }
+
+        if(dp[n]!=-1){
+            return dp[n];
+        }
+
+        dp[n] = climb(n-1,dp)+climb(n-2,dp);
+        return dp[n];
+    }
+
+    int climbStairs(int n) {
+        vector<int> dp(n+1,-1);
+        return climb(n,dp);
+    }
+};
