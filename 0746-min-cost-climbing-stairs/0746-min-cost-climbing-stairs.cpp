5b0918250e55f5233a093909028f62b83c7e59bd
rrohitmeenaa <rohitmeena1789@gmail.com>
2026-09-10 15:35:17 +0530

    Time: 0 ms (100%), Space: 17.8 MB (10.21%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -1,17 +1,25 @@
+// i can start either with step 1 or step 2 and i have to return the min cost 
+// here dp[i] means the min cost to reach stair i .
 class Solution {
 public:
+    int solve(int idx,vector<int>& dp,vector<int>& cost){
+        int n = cost.size();
+        if(idx>=n){
+            return 0;
+        }
+        if(dp[idx]!=-1){
+            return dp[idx];
+        }
+        int left = cost[idx] + solve(idx+1,dp,cost) ;
+        int right = cost[idx] + solve(idx+2,dp,cost);
+
+        return dp[idx] = min(left,right);
+
+    }
+
     int minCostClimbingStairs(vector<int>& cost) {
         int n = cost.size();
-
-        vector<int> dp(n);
-
-        dp[0] = cost[0];
-        dp[1] = cost[1];
-
-        for(int i=2;i<n;i++){
-            dp[i] = cost[i] + min(dp[i-1],dp[i-2]);
-        }
-
-        return min(dp[n-1],dp[n-2]);
+        vector<int> dp(n+1,-1);
+        return min(solve(0,dp,cost),solve(1,dp,cost));
     }
 };
