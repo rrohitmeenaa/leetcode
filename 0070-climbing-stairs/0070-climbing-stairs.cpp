08f748d27fdd30e75fdd0d161c37216810d165f6
rrohitmeenaa <rohitmeena1789@gmail.com>
2026-09-15 21:46:17 +0530

    Time: 0 ms (100%), Space: 8.6 MB (20.2%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,20 +1,19 @@
+// solving this problem using tabulation method of dp 
+
 class Solution {
 public:
-    int solve(int n,vector<int>& dp){
-        if(n==0 || n==1){
-            return 1;
-        }
-        if(dp[n]!=-1){
-            return dp[n];
-        }
-        int left = solve(n-1,dp);
-        int right = solve(n-2,dp);
-
-        return dp[n] =  left + right;
-    }
-
     int climbStairs(int n) {
+        // initializing a dp vector to store the no. of ways to reach index i from the starting index . 
         vector<int> dp(n+1,-1);
-        return solve(n,dp);
+
+        // base cases 
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for(int i =2;i<=n;i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+
+        return dp[n];
     }
 };
