3f3b0acd4f2404704e13086ceafe85400d6107a7
rrohitmeenaa <rohitmeena1789@gmail.com>
2026-09-20 12:58:43 +0530

    Time: 1 ms (6.36%), Space: 8.5 MB (53.05%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,19 +1,25 @@
-// solving this problem using tabulation method of dp 
+// i have to solve the climibing stairs question with all the methods of solving the dp questions which is memoization tabulation and space optimization 
+
+// memoization
 
 class Solution {
 public:
-    int climbStairs(int n) {
-        // initializing a dp vector to store the no. of ways to reach index i from the starting index . 
-        vector<int> dp(n+1,-1);
-
-        // base cases 
-        dp[0] = 1;
-        dp[1] = 1;
-
-        for(int i =2;i<=n;i++){
-            dp[i] = dp[i-1] + dp[i-2];
+    int solve(int idx,vector<int>& dp){
+        if(idx==0 || idx==1){
+            return 1;
+        }
+        if(dp[idx]!=-1){
+            return dp[idx];
         }
 
-        return dp[n];
+        int one = solve(idx-1,dp);
+        int two = solve(idx-2,dp);
+
+        return dp[idx] = one + two;
+    }
+
+    int climbStairs(int n) {
+        vector<int> dp(n+1,-1);
+        return solve(n,dp);
     }
 };
